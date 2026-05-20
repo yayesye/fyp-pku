abca4abb6141d4aedbd0ea7e1482a3618272191f
yayesye <061106emir@gmail.com>
2026-05-20 17:13:13 +0800

    Revert "Added RealTime Update for Bulletin Posts"
    This reverts commit 07692bcdf328a968aeec36ac692c839bf705c324.

--- src/non-page-components/supabaseDB.jsx
+++ src/non-page-components/supabaseDB.jsx
@@ -19,16 +19,19 @@ export const supabase = createClient(supabaseUrl, supabaseKey, {
 })
 
 
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient('https://<project-ref>.supabase.co', '<anon-or-publishable-key>')
-
-const channel = supabase
-  .channel('bulletin_posts:global', { config: { private: true } })
-  .on('broadcast', { event: 'INSERT' }, (payload) => console.log('insert', payload))
-//   .on('broadcast', { event: 'UPDATE' }, (payload) => console.log('update', payload))
-  .on('broadcast', { event: 'DELETE' }, (payload) => console.log('delete', payload))
-  .subscribe()
+const subscription = supabase.channel('public-posts')
+  .on(
+    'postgres_changes',
+    { 
+      event: 'INSERT', 
+      schema: 'public', 
+      table: 'BulletinPosts' 
+    },
+    (payload) => {
+      console.log('New post received!', payload.new)
+      // Update your local state here with payload.new
+    }
+  ).subscribe()
 
 
 
